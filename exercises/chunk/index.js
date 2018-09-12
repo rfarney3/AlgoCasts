b9470e74c576974a2fae926f320f92120e59bef3
rfarney3 <farneyr@yahoo.com>
2018-09-11 18:36:53 -0700

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,57 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //declaring variable 'chunked' as an empty array
+  let chunked = []
+
+  //looping through the array until it has been entirely "manipulated" or split into our subarrays
+  while(array.length > 0) {
+    //taking the spliced segments completely out of our original array
+    //pushing these subarrays into our new "chunked" array
+    chunked.push(array.splice(0, size))
+  }
+  //returning the new array of subarrays
+  return chunked
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+  //declaring variable 'chunked' as an empty array
+  // let chunked = []
+//
+//   //setting our start point for our while loop at index 0
+//   let i = 0;
+//   //looping through the array until we have reached the final index
+//   while (i < array.length) {
+//     //push the sliced subarray of length 'size' into our new 'chunked' array
+//     chunked.push(array.slice(i, i + size))
+//     //increment by the size as to avoid duplicates
+//     i += size;
+//   }
+//   //return the array of subarrays
+//   return chunked
+// }
+
+// function chunk(array, size) {
+//   //declaring variable 'chunked' as an empty array
+//   let chunked = [];
+//
+//   //for loop iterating through every element of our input array
+//   for (let ele of array) {
+//     //declaring variable 'last' as the last index of our 'chunked' array
+//     const last = chunked[chunked.length-1];
+//
+//     //checking if last is undefined or if the last subarray is equal to the size
+//     if (!last || last.length === size) {
+//       //then we push the element to be a new subarray in 'chunked'
+//       chunked.push([ele])
+//     } else {
+//       //if not, then we add the element to the 'last' subarray
+//       last.push(ele)
+//     }
+//   }
+//   //return the array of subarrays
+//   return chunked
+// }
